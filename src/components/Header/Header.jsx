acedc1870b0c38288c3fa3597ab21800fc8c6f99
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 00:30:02 +0100

    created component for page header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from "./Header.module.scss";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className={styles.Header}>
+      </header>
+    );
+  }
+}
+
+export default Header;
