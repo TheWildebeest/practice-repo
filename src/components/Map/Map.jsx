2f38234ef91ba61f67d23c303ccad45a3f952cfb
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 00:30:26 +0100

    created Map component

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import styles from "./Map.module.scss";
+
+class Map extends Component {
+  render() {
+    return (
+      <map id={styles.Map}>
+      </map>
+    );
+  }
+}
+
+export default Map;
