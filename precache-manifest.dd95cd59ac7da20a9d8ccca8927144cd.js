b1408b2ea9d63337dacdcda8a04d720d23fe70e6
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 19:21:14 +0100

    Updates

---
+++ precache-manifest.dd95cd59ac7da20a9d8ccca8927144cd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85024646b2459a395bf197a9378959c9",
+    "url": "/omnibus-app/index.html"
+  },
+  {
+    "revision": "265a4f8eb256a2370596",
+    "url": "/omnibus-app/static/css/main.0aabeefc.chunk.css"
+  },
+  {
+    "revision": "047ddd7ecf11b599ca21",
+    "url": "/omnibus-app/static/js/2.6b651931.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/omnibus-app/static/js/2.6b651931.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265a4f8eb256a2370596",
+    "url": "/omnibus-app/static/js/main.ad14920f.chunk.js"
+  },
+  {
+    "revision": "9991de91058e175d1308",
+    "url": "/omnibus-app/static/js/runtime-main.d7c700bf.js"
+  },
+  {
+    "revision": "f0073cc25e4b838865f0959563092f37",
+    "url": "/omnibus-app/static/media/background.f0073cc2.jpg"
+  },
+  {
+    "revision": "8999d3eeab89336a0a09496112a91680",
+    "url": "/omnibus-app/static/media/honk.8999d3ee.mp3"
+  },
+  {
+    "revision": "7b591356328381990da1f6d3a1684872",
+    "url": "/omnibus-app/static/media/logo.7b591356.png"
+  },
+  {
+    "revision": "08a8122edec5cbcef8ada2783ed08125",
+    "url": "/omnibus-app/static/media/unibody.08a8122e.ttf"
+  },
+  {
+    "revision": "a20cbfc1ecc990d0f6ed69b225664f09",
+    "url": "/omnibus-app/static/media/unibody.a20cbfc1.woff"
+  },
+  {
+    "revision": "cd7fb8366edeb6248392cc81e1a03564",
+    "url": "/omnibus-app/static/media/unibody.cd7fb836.woff2"
+  }
+]);
