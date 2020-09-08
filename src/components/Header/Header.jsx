c49f3388e047c228c5dcfde49cbba22d82f9c6fc
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 01:20:54 +0100

    basic styling/branding

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import styles from "./Header.module.scss";
+import logo from '../../assets/images/logo.png';
 
 class Header extends Component {
   render() {
     return (
       <header className={styles.Header}>
+        <h1 data-subheading={"departures near you"}>OMNI<span className={styles.orange}>BUS </span></h1>
       </header>
     );
   }
