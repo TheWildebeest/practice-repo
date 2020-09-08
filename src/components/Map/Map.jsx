ef848d4b41457fe5bc012649741702db438d83f9
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 09:43:50 +0100

    map is rendering

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import styles from "./Map.module.scss";
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
+
+  defaultLocation = {
+    address: 'London',
+    lat: 51.5074,
+    lng: 0.1278,
+  }
   render() {
     return (
       <section id={styles.Map}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyB9SARU4o0HtipTD3Z6qIqDQ5lg9ZRp_ok' }}
+          defaultCenter={this.defaultLocation}
+          defaultZoom={12}
+        >
+
+        </GoogleMapReact>
       </section>
     );
   }
