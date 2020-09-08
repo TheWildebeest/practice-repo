77d95f2b4d4b69fea9b0b3a183ed774f688cd564
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 13:23:30 +0100

    clean google maps DD data

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,19 @@ class App extends Component {
   }
 
   setLocationState = (lat, lng) => {
-    this.setState({ latitude: lat, longitude: lng })
+    this.setState({ latitude: Number(Math.round(lat + 'e6') + 'e-6'), longitude: Number(Math.round(lat + 'e6') + 'e-6') })
   }
 
-  fetchDepartures = (lat, lng) => {
+  fetchDepartures = () => {
     const key = "7c3d3e46d0cc641a20988d30562ee5fc";
     const ID = "401dd4f8";
-    console.log(lat, lng, key, ID);
+    const { lat, lng } = this.state;
+    fetch(`http://transportapi.com/v3/uk/places.json?places.json?&lon=-0.006944&type=bus_stop&app_id=401dd4f8&app_key=7c3d3e46d0cc641a20988d30562ee5fc&lat=51.534121`)
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log(prevState.latitude + this.state.latitude);
+    console.log(prevState.latitude, this.state.latitude);
     if ((this.state.latitude !== prevState.latitude) || (this.state.longitude !== prevState.longitude)) {
-      this.fetchDepartures(this.state.latitude, this.state.longitude);
+      this.fetchDepartures();
     }
   }
 
