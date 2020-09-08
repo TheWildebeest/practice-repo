d00ea39054969e30ee60ac23d8bac787a76e65de
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 16:13:42 +0100

    second fetch() call successful

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,21 @@ class App extends Component {
     this.state = {
       latitude: 51.489165,
       longitude: -0.142499,
-      atcoCode: 0
-    };
-  }
+      stopName: "",
+      departures: [
+        {
+          line: "",
+          direction: "",
+          expectedDepartureTime: ""
+        },
+        {
+          line: "",
+          direction: "",
+          expectedDepartureTime: ""
+        },
+      ]
+    }
+  };
 
   setLocationState = (lat, lng) => {
     this.setState({ latitude: Number(Math.round(lat + 'e6') + 'e-6').toString(), longitude: Number(Math.round(lng + 'e6') + 'e-6').toString() })
@@ -24,11 +36,16 @@ class App extends Component {
     const { latitude, longitude } = this.state;
     fetch(`http://transportapi.com/v3/uk/places.json?places.json?&app_id=${ID}&app_key=${key}&lat=${latitude}&lon=${longitude}&type=bus_stop`)
       .then(response => response.json())
-      .then(data => this.setState({ atcoCode: data.member[0].atcocode }))
-      .then()
+      .then(data => data.member[0].atcocode)
+      .then(atcoCode => {
+        return fetch(`http://transportapi.com/v3/uk/bus/stop/${atcoCode}/live.json?app_id=${ID}&app_key=${key}&group=route&limit=1&nextbuses=no`)
+          .then(response => response.json())
+      })
+      .then(data => this.setState({ departures: Object.entries(data.departures) }))
       .catch(error => alert(error.message));
   }
-  componentDidUpdate(prevProps, prevState) {
+
+  componentDidUpdate(_prevProps, prevState) {
     if ((this.state.latitude !== prevState.latitude) || (this.state.longitude !== prevState.longitude)) {
       this.fetchAtcoCode();
       console.log("Bus stop updated");
@@ -36,11 +53,12 @@ class App extends Component {
   }
 
   render() {
+    const { stopName, departures } = this.state;
     return (
       <div className={styles.App} >
         <Header />
         <main className={styles.Main}>
-          <Departures />
+          <Departures stopName={stopName} departures={departures} />
           <Map setLocationState={this.setLocationState} />
         </main>
       </div>
--- src/components/Departures/Departures.jsx
+++ src/components/Departures/Departures.jsx
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import styles from "./Departures.module.scss";
 
 class Departures extends Component {
+
   render() {
+    const { stopName, departures } = this.props;
     return (
       <section className={styles.DeparturesSection}>
+        <div className={styles.DeparturesTitle}>
+          <h2 className={styles.stopName}>{stopName ? stopName : "Click anywhere on the map to get started"}</h2>
+        </div>
         <div>
-          <hgroup>
-            {/* Destinations */}
+          <hgroup className={styles.DeparturesList}>
+            {/* {departures[0].line ? "Live departures!" : "Live departures will show here"} */}
+            {"Live departures will show here!"}
           </hgroup>
         </div>
       </section>
