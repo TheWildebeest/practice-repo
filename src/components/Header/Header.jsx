83d920211102855baca24c04fc843ea352a4bf76
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-08 01:45:48 +0100

    added easter egg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import styles from "./Header.module.scss";
 import logo from '../../assets/images/logo.png';
+import honk from '../../assets/sounds/honk.mp3'
 
 class Header extends Component {
+
+  playHonk = () => {
+    const sound = new Audio(honk);
+    sound.play();
+  }
   render() {
     return (
       <header className={styles.Header}>
+        <img src={logo} alt="omnibus logo" onClick={this.playHonk} />
         <h1 data-subheading={"departures near you"}>OMNI<span className={styles.orange}>BUS </span></h1>
       </header>
     );
